ba1add79afa0f12647720dc3918f83e5109afcab
PSSTanusha <psstanusha@gmail.com>
2018-12-14 18:30:48 +0530

    Create input.js

---
+++ Node/Babel/input.js
@@ -0,0 +1,9 @@
+var map = new Map();
+map.set('name', 'John');
+map.set('id', 10);
+map.size; // 2. Returns the size of the map.
+map.keys(); // outputs only the keys. 
+map.values(); // outputs only the values.
+for (let key of map.keys()) {
+ console.log(key);
+}
